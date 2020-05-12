290f7da7968e348321915ca0b0b80a1b71665ce2
Gregory <Lenartowicz>
2020-05-12 11:52:42 +0200

    even nombers

---
+++ Desktop/Projekty python/Projekt próbny/Exercise_1 May 2020.py
@@ -0,0 +1,14 @@
+
+for i in range(1,3):
+    if i %2==0:
+        print('first way', i, end=' ')
+
+def EvenNumbersOnly(a,b):
+    for i in range(a+1 if a%2 else a, b+1,2):
+        print('second day', i, end=' ')
+
+
+a,b = 1,2
+EvenNumbersOnly(a,b)
+
+
