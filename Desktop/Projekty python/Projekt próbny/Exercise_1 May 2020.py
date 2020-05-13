0e66ec0bd4658f8dffac0431a66ac9c9d5da46e4
Gregory <Lenartowicz>
2020-05-13 09:22:20 +0200

    exercises: adding items

--- Desktop/Projekty python/Projekt próbny/Exercise_1 May 2020.py
+++ Desktop/Projekty python/Projekt próbny/Exercise_1 May 2020.py
@@ -1,15 +1,15 @@
-### exercises for loops
+### exercise: adding items to the list
 
-for i in range(1,3):
-    if i %2==0:
-        print('first way', i, end=' ')
+a = [1,2,4,6,8]
+# the sum is to be equal to 7
 
-def EvenNumbersOnly(a,b):
-    for i in range(a+1 if a%2 else a, b+1,2):
-        print('second day', i, end=' ')
+b = a[0]+a[3]
+print(b)
+
+a.append(a[len(a)-2]+a[len(a)-5])
+
+print(a[5])
 
 
-a,b = 1,2
-EvenNumbersOnly(a,b)
 
 
