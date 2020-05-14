641a1c28b8972da7ebe439fb271b43d33850cbe6
Gregory <Lenartowicz>
2020-05-14 11:39:52 +0200

    adding an exercise ( drawing a square)

--- Desktop/Projekty python/Projekt próbny/Exercise_2 Maj 2020.py
+++ Desktop/Projekty python/Projekt próbny/Exercise_2 Maj 2020.py
@@ -5,21 +5,33 @@ import random
 #random choice
 s = random.randint(97,97+25)
 letter = chr(s)
-print(letter)
+#print(letter)
 
 #random letter from the generated alphabet
-
 alphabet = []
 for i in range(97,97+26):
     p = chr(i)
     alphabet += p
 #    print(alphabet, end=' ')
 rand_let = random.choice(alphabet)
-print(rand_let)
+#print(rand_let)
 
-print(chr(97+25))
+# different approach
+q = ('dfgfdsgargarcvfergserh')
+w = random.choice(q)
+print(w)
+print(len(q))
 
+# check if an element exists in the set
+print('v' in q)
 
+# drawing a square random size
+n = random.randint(4,15)
+print(n)
+#for i in range(1,n+1):  # height (y axis)
+#    print(w*n)            # width  (x axis)
 
-
-
+# drawing an empty square (in prpgress)
+for i in range(1,n+1):
+#    print(w, w if n<0 else '1'* (n-2),w)
+    print(w, w*(n-2) if i in (2,0) else ' '* (n-2),w)
